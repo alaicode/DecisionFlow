2d182d21c2d2a3d59864b2ccb7ef5fcbf527ec3c
alaicode <randrianarimanana.toki@gmail.com>
2026-07-01 00:32:08 +0400

    add basis domain with buisiness rules

---
+++ DecisionFlow.Domain/Entities/DecisionStatues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionFlow.Domain.Entities
+{
+    public enum DecisionStatues
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+}
