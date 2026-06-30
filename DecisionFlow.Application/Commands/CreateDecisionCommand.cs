176016e3d58f369e0e238b4dc68d809b96d16c14
alaicode <randrianarimanana.toki@gmail.com>
2026-07-01 01:08:14 +0400

    add basis use case

---
+++ DecisionFlow.Application/Commands/CreateDecisionCommand.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionFlow.Application.Commands
+{
+    public record CreateDecisionCommand(string Title, string Description);
+}
---
+++ DecisionFlow.Application/Interfaces/IDecisionRepository.cs
@@ -0,0 +1,16 @@
+﻿using DecisionFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionFlow.Application.Interfaces
+{
+    public interface IDecisionRepository
+    {
+        Task<Decision> GetByIdAsync(Guid id);
+        Task<List<Decision>> GetAllAsync();
+        Task AddAsync(Decision decision);
+        Task SaveChangesAsync();
+
+    }
+}
